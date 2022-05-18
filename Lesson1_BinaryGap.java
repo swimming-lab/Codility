a248f353556722bb797ea965b80fd01442684757
swimming <84673254+swimming-lab@users.noreply.github.com>
2022-05-18 22:55:49 +0900

    코딩테스트 결과 업로드

---
+++ Lesson1_BinaryGap.java
@@ -0,0 +1,36 @@
+public class Lesson1_BinaryGap {
+    public int solution(int N) {
+        // write your code in Java SE 8
+        char[] binaries = Integer.toBinaryString(N).toCharArray();
+        System.out.println("start~!");
+        System.out.println("N =" + N);
+
+        int result = 0;
+        int startIndex = 0;
+        for(int i=0; i<binaries.length; i++) {            
+            if ((binaries[i]-'0') == 1 && startIndex == 0) {
+                startIndex = i + 1;
+                System.out.println("1. startIndex =" + startIndex);
+            } else if ((binaries[i]-'0') == 1 && startIndex > 0) {
+                if (result < (i - startIndex)) {
+                    result = i - startIndex;
+                    System.out.println("result =" + result);
+                }
+                startIndex = i + 1;
+                System.out.println("2. startIndex =" + startIndex);
+            }
+        }
+
+
+        System.out.println("result =" + result);
+        System.out.println("end~!");
+        return result;
+    }
+
+    public static void main(String[] args) {
+        final Lesson1_BinaryGap solution = new Lesson1_BinaryGap();
+        solution.solution(1042);
+    }
+}
+
+
---
+++ Lesson2_CyclicRotation.java
@@ -0,0 +1,34 @@
+public class Lesson2_CyclicRotation {
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+
+        int shift = 0;
+        if (A.length == 1 || A.length == 0) {
+            shift = 0;
+        } else if (A.length > K) {
+            shift = K;
+        } else {
+            shift = K % A.length;
+        }
+
+        int[] result = new int[A.length];
+        for(int i=0; i<A.length; i++) {
+            int position = i + shift;
+            if (position >= A.length) {
+                position = position - A.length;
+            }
+
+            result[position] = A[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        final Lesson2_CyclicRotation solution = new Lesson2_CyclicRotation();
+
+        solution.solution(new int[]{}, 0);
+    }
+}
+
+
