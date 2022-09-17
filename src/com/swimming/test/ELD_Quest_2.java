b54f4c122962e037b6e481fee1fb1921fdbb71f9
swimming <sooyoung209@naver.com>
2022-09-17 23:07:06 +0900

    programmers test 문제 풀이

--- src/com/swimming/test/ELD_Quest_2.java
+++ src/com/swimming/test/ELD_Quest_2.java
@@ -17,27 +17,72 @@ public class ELD_Quest_2 {
     public long solution(int n, int r, int c) {
         long answer = 0;
 
-        /**
-         * 세로 공식
-         * 1 + 2*1 (1)
-         * 3 + 1   (2)
-         * 4 + 2*3 (3)
-         * 10 + 1  (4)
-         * 11 + 2*5(5)
-         * 21 + 1  (6)
-         * 22 + 2*7(7)
-         * 36
-         *
-         * 가로 공식
-         * 1 + 1   (1)
-         * 2 + 2*2 (2)
-         * 6 + 1   (3)
-         * 7 + 2*4 (4)
-         * 15 + 1  (5)
-         * 16 + 2*6(6)
-         * 28 + 1  (7)
-         * 29
-         */
+        long[][] array = new long[n][n];
+        long count = 1;
+        long total = n * n;
+        int x = 0;
+        int y = -1;
+        int upOrDown = 1;
+
+        while (count <= total) {
+            if (x == n - 1) {
+                array[++y][x] = count++;
+                upOrDown = 0;
+
+                if (y == (r-1) && x == (c-1)) {
+                    answer = array[y][x];
+                    break;
+                }
+            }
+
+            if (y == n - 1) {
+                array[y][++x] = count++;
+                upOrDown = 1;
+
+                if (y == (r-1) && x == (c-1)) {
+                    answer = array[y][x];
+                    break;
+                }
+            }
+
+            if (y == 0) {
+                array[y][++x] = count++;
+                upOrDown = 0;
+
+                if (y == (r-1) && x == (c-1)) {
+                    answer = array[y][x];
+                    break;
+                }
+            }
+
+            if (x == 0) {
+                array[++y][x] = count++;
+                upOrDown = 1;
+
+                if (y == (r-1) && x == (c-1)) {
+                    answer = array[y][x];
+                    break;
+                }
+            }
+
+            if (upOrDown == 1 && x != n - 1 && y != 0) {
+                array[--y][++x] = count++;
+
+                if (y == (r-1) && x == (c-1)) {
+                    answer = array[y][x];
+                    break;
+                }
+            }
+
+            if (upOrDown == 0 && y != n - 1 && x != 0) {
+                array[++y][--x] = count++;
+
+                if (y == (r-1) && x == (c-1)) {
+                    answer = array[y][x];
+                    break;
+                }
+            }
+        }
 
         return answer;
     }
