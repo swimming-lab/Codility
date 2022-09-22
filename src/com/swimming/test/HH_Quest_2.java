e11e5f0894267f45585cb98327be9f367217efc1
swimming <sooyoung209@naver.com>
2022-09-22 23:45:42 +0900

    programmers coding test

---
+++ src/com/swimming/test/HH_Quest_2.java
@@ -0,0 +1,17 @@
+package com.swimming.test;
+
+import java.util.*;
+
+public class HH_Quest_2 {
+    public int solution(int[] scoures, int k) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        final HH_Quest_2 solution = new HH_Quest_2();
+        int result = solution.solution(new int[]{1 , 3, 7, 8, 10, 15}, 5);
+
+        System.out.println(result);
+//        System.out.println(Arrays.toString(result));
+    }
+}
